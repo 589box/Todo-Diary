81dc1d816be83d24850f64ca1cbe8595d513ba83
Copal <103774029+589box@users.noreply.github.com>
2022-09-07 22:08:33 +0900

    Delete mission6 directory

--- mission6/js/preview.js
+++
@@ -1,7 +0,0 @@
-function previewImg(obj){
-    var fileReader = new FileReader();
-	fileReader.onload = (function() {
-		document.getElementById('preview').src = fileReader.result;
-	});
-	fileReader.readAsDataURL(obj.files[0]);
-}
